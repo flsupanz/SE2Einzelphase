691d44faa9d89b67d19ecf3254dae68f46b104ec
floriansupanz <florian.supanz@gmail.com>
2020-03-16 21:03:09 +0100

    Server Verbindung hergestellt und Aufgabe abgeschlossen

--- app/src/main/java/com/example/einfhrungsbeispiel/Client.java
+++ app/src/main/java/com/example/einfhrungsbeispiel/Client.java
@@ -1,4 +1,41 @@
 package com.example.einfhrungsbeispiel;
 
-public class Client {
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client extends AsyncTask<String,Void,String> {
+
+
+
+        Socket socket;
+        BufferedReader bufferedReader;
+        PrintWriter pw;
+        String response;
+
+        @Override
+        protected String doInBackground(String... voids) {
+
+        try {
+             socket = new Socket("se2-isys.aau.at", 53212);
+             pw = new PrintWriter((socket.getOutputStream()), true);
+             pw.write(voids[0]);
+             bufferedReader = new BufferedReader((new InputStreamReader(socket.getInputStream())));
+             response  = bufferedReader.readLine();
+
+             onPostExecute(response);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
+
--- app/src/main/java/com/example/einfhrungsbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einfhrungsbeispiel/MainActivity.java
@@ -10,6 +10,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
 
 import java.util.stream.IntStream;
@@ -17,79 +25,128 @@ import java.util.stream.IntStream;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn;
     private EditText EditTextMtrnmr;
-    private TextView textView;
+    private TextView Ergebnis;
+
+
+
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btn =(Button) findViewById(R.id.button);
         EditTextMtrnmr = (EditText) findViewById(R.id.editText2);
+        Ergebnis =(TextView) findViewById((R.id.textView));
 
         btn.setOnClickListener(this);
+        //Thread myThread= new Thread( new ServerThread());
 
 
-    }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+        }
+   /* class ServerThread implements Runnable{
+        Socket socket;
+        ServerSocket serverSocket;
+        InputStreamReader isr;
+        BufferedReader bufferedReader;
+        String input;
+        @Override
+        public void run(){
+            try {
+                serverSocket = new ServerSocket(53212);
+                while (true){
+                    socket=serverSocket.accept();
+                    isr = new InputStreamReader((socket.getInputStream()));
+                    bufferedReader=new BufferedReader(isr);
+                    input =bufferedReader.readLine();
+
+
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }*/
+
+
+
+
+
+
+   @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
 
-        if(v == btn){
+
+
+
+
+        Client client = new Client();
+        client.execute(EditTextMtrnmr.getText().toString());
+
+
+
+
+        if (v == btn) {
             int matrikelnummer;
-            matrikelnummer= Integer.valueOf(EditTextMtrnmr.getText().toString());
-             String temp = Integer.toString(matrikelnummer);
+            matrikelnummer = Integer.valueOf(EditTextMtrnmr.getText().toString());
+            String temp = Integer.toString(matrikelnummer);
             int[] a = new int[temp.length()];
-            for (int i = 0; i < temp.length(); i++)
-            {
+            for (int i = 0; i < temp.length(); i++) {
                 a[i] = temp.charAt(i) - '0';
             }
 
             Arrays.sort(a);
-            int countOdd=0;
-            int countEven=0;
+            int countOdd = 0;
+            int countEven = 0;
             for (int i = 0; i < a.length; ++i) {
-                if(a[i]%2==0){
+                if (a[i] % 2 == 0) {
                     ++countEven;
-                }
-                else{
+                } else {
                     countOdd++;
 
                 }
             }
-            int[] evenArray= new int[countEven];
-            int[] oddArray= new int [countOdd];
-            int j=0;
-            int y=0;
-
+            int[] evenArray = new int[countEven];
+            int[] oddArray = new int[countOdd];
+            int j = 0;
+            int y = 0;
 
 
             for (int i = 0; i < a.length; ++i) {
 
-                if(a[i]%2==0){
-                    evenArray[j]=a[i];
+                if (a[i] % 2 == 0) {
+                    evenArray[j] = a[i];
                     j++;
 
-                }
-                else{
-                    oddArray[y]=a[i];
+                } else {
+                    oddArray[y] = a[i];
                     y++;
                 }
             }
 
-            a= IntStream.concat(IntStream.of(evenArray),IntStream.of(oddArray)).toArray();
+            a = IntStream.concat(IntStream.of(evenArray), IntStream.of(oddArray)).toArray();
 
             StringBuilder sb = new StringBuilder();
-            for(int i : a) {
+            for (int i : a) {
                 sb.append(i);
             }
-            int ergebnis = Integer.parseInt(sb.toString());
+            String ergebnis = sb.toString();
 
-            textView.setText("Ergebnis: " + ergebnis);
+            client.response=ergebnis;
 
+            Ergebnis.setText(client.response);
+
+
+
+
+
+        }
 
     }
 
-}}
+
+}
+
+
